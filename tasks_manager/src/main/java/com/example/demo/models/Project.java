7358e46d760a2cbc59f8aee64b1a963505cdd698
JacopoTrincanato <trincanatoj@gmail.com>
2026-02-24 16:10:37 +0100

    create entities, enums, repositories and entity relationships

---
+++ tasks_manager/src/main/java/com/example/demo/models/Project.java
@@ -0,0 +1,97 @@
+package com.example.demo.models;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="project")
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(min = 5, max = 30, message = "Name filed cannot be shorter than 5 characters and longer than 30")
+	@NotBlank(message = "Name field value cannot be blank, empty or null")
+	private String name;
+	
+	@Size(min = 10, max = 250, message = "Description filed cannot be shorter than 10 characters and longer than 250")
+	@NotBlank(message = "Description field value cannot be blank, empty or null")
+	private String description;
+	
+	@NotNull
+	private LocalDate creationDate;
+	
+	@OneToMany(mappedBy = "project")
+	List<Task> tasks;
+	
+	@OneToMany(mappedBy = "project")
+	List<User> users;
+	
+	public void addTask(Task t) {
+		tasks.add(t);
+	}
+	
+	public void removeTask(Task t) {
+		tasks.remove(t);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDate creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+}
+
---
+++ tasks_manager/src/main/java/com/example/demo/models/Task.java
@@ -0,0 +1,124 @@
+package com.example.demo.models;
+
+import java.time.LocalDate;
+
+import com.example.demo.enums.Priority;
+import com.example.demo.enums.Status;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="tasks")
+public class Task {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(min = 5, max = 30, message = "Title filed cannot be shorter than 5 characters and longer than 30")
+	@NotBlank(message = "Title field value cannot be blank, empty or null")
+	private String title;
+	
+	@Size(min = 10, max = 250, message = "Description filed cannot be shorter than 10 characters and longer than 250")
+	@NotBlank(message = "Description field value cannot be blank, empty or null")
+	private String description;
+	
+	@Enumerated(EnumType.STRING)
+	private Status status;
+	
+	@Enumerated(EnumType.STRING)
+	private Priority priority;
+	
+	@NotNull
+	private LocalDate deadLine;
+	
+	@ManyToOne
+	@JoinColumn(name = "project_id", nullable = false)
+	private Project project;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	public boolean isScaduto() {
+		return this.deadLine.isBefore(LocalDate.now());
+	}
+	
+	public void complete() {
+		this.status = Status.DONE;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Priority getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Priority priority) {
+		this.priority = priority;
+	}
+
+	public LocalDate getDeadLine() {
+		return deadLine;
+	}
+
+	public void setDeadLine(LocalDate deadLine) {
+		this.deadLine = deadLine;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
