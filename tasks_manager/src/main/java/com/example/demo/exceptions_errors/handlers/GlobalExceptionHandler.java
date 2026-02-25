4998e769bd5bdbcb45f74e5674689d4e4015401b
JacopoTrincanato <trincanatoj@gmail.com>
2026-02-25 15:36:54 +0100

    update ProjectController e create new exceptions in GlobalExceptionHandler

--- tasks_manager/src/main/java/com/example/demo/exceptions_errors/handlers/GlobalExceptionHandler.java
+++ tasks_manager/src/main/java/com/example/demo/exceptions_errors/handlers/GlobalExceptionHandler.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.demo.exceptions_errors.ErrorResponse;
 import com.example.demo.exceptions_errors.ProjectNotFoundException;
+import com.example.demo.exceptions_errors.TaskNotFoundException;
+import com.example.demo.exceptions_errors.UserNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +19,17 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<?> handleProjectNotFoundException(ProjectNotFoundException exception) {
 		ErrorResponse projectNotFound = new ErrorResponse(LocalDateTime.now(), exception.getMessage(), "Project not found");
 		return new ResponseEntity<>(projectNotFound, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(TaskNotFoundException.class)
+	public ResponseEntity<?> handleTaskNotFoundException(TaskNotFoundException exception) {
+		ErrorResponse taskNotFound = new ErrorResponse(LocalDateTime.now(), exception.getMessage(), "Task not found");
+		return new ResponseEntity<>(taskNotFound, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException exception) {
+		ErrorResponse userNotFound = new ErrorResponse(LocalDateTime.now(), exception.getMessage(), "User not found");
+		return new ResponseEntity<>(userNotFound, HttpStatus.NOT_FOUND);
 	}
 }
--- tasks_manager/src/main/java/com/example/demo/restControllers/ProjectRestController.java
+++ tasks_manager/src/main/java/com/example/demo/restControllers/ProjectRestController.java
@@ -3,15 +3,22 @@ package com.example.demo.restControllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.create.ProjectCreateDTO;
 import com.example.demo.dto.response.ProjectResponseDTO;
 import com.example.demo.pagination.ContentPagination;
 import com.example.demo.services.ProjectService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/projects")
@@ -29,7 +36,7 @@ public class ProjectRestController {
 	}
 	
 	@GetMapping("/id/{id}")
-	public ResponseEntity<?> getProjectById(@PathVariable("id") Long id) {
+	public ResponseEntity<?> getProjectById(@PathVariable("id") @Valid Long id) {
 
 		ProjectResponseDTO project = projectService.findById(id);
 		return new ResponseEntity<>(project, HttpStatus.OK);
@@ -37,10 +44,30 @@ public class ProjectRestController {
 	}
 	
 	@GetMapping("/title/{title}")
-	public ResponseEntity<?> getProjectByTitle(@PathVariable("title") String title) {
+	public ResponseEntity<?> getProjectByTitle(@PathVariable("title") @Valid String title) {
 
 		ProjectResponseDTO project = projectService.findByTitle(title);
 		return new ResponseEntity<>(project, HttpStatus.OK);
 
 	}
+	
+	@PostMapping
+    public ResponseEntity<?> createProject(@Valid @RequestBody ProjectCreateDTO project) {
+
+        return new ResponseEntity<>(projectService.create(project), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProject(@Valid @RequestBody ProjectCreateDTO project, @Valid @PathVariable Long id) {
+
+        return new ResponseEntity<>(projectService.update(project, id), HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProject(@Valid @PathVariable Long id) {
+
+        projectService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
